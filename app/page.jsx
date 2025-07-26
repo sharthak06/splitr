b939edb31dc3020a5ca943e9ba36903f5ab716d4
sharthak singh <sharthaksingh06@gmail.com>
2025-07-26 13:00:30 +0530

    connected inngest and defined schema

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,27 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="pt-16">
-      Hello
-      <br />
-      
+   
+       <div className="flex flex-col pt-16">
+      {/* ───── Hero ───── */}
+      <section className="mt-20 pb-12 space-y-10 md:space-y-15 px-5">
+        <div className="container mx-auto px-4 md:px-6 text-center space-y-6">
+          <Badge variant="outline" className="bg-green-100 text-green-700">
+            Split expenses. Simplify life.
+          </Badge>
+           <h1 className="gradient-title mx-auto max-w-6xl text-4xl font-bold md:text-8xl">
+            The smartest way to split expenses with friends
+          </h1>
+            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed">
+            Track shared expenses, split bills effortlessly, and settle up
+            quickly. Never worry about who owes who again.
+          </p>
+          
+          </div>
+      </section>
     </div>
   );
 }
